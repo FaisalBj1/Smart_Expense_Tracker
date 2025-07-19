0c0cea7a6b37ce68f3dd39233d48f866b540bc73
Faisal Banjarr <faisalbj1@Faisals-MacBook-Pro.local>
2025-07-19 03:07:26 -0700

    crearing expense.py

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print("Hello, World")
+
+
+
+def main():
+    while True:
+        print("1. Add expense")
+        print("2. View expenses")
+        print("3. Delete expense")
+        print("4. Monthly summary")
+        print("5. Exit")
+
+        break
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
